717408ce433c2e5d07b62a5c8e34ffcdb24dba19
Oleg Belov <belov@ymail.com>
2023-06-12 16:53:55 -0300

    Create current-date-format.ps1

---
+++ current-date-format.ps1
@@ -0,0 +1,15 @@
+# Current date
+$curDate = Get-Date -format "MM-dd-yyyy HH:MM:ss"
+$curDate 
+pause
+
+$curDate = Get-Date -format "MM-dd-yyyy"
+$curDate 
+
+$curLogDate = Get-Date -format "yyMMdd"
+$curLogDate
+
+$StartDate = (Get-Date "01.10.2020").AddDays(0) #start date
+
+$curDate = Get-Date -format "dd-MM-yyyy"
+$curDate 
